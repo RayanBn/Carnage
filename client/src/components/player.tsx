cb5e2a34733cd83251d54d032b347d5641f3b625
RayanBn <72317838+RayanBn@users.noreply.github.com>
2025-04-23 11:13:46 +0900

    feat(player): enhance player component by adding readiness indicator with visual feedback and integrating player state management

--- client/src/components/player.tsx
+++ client/src/components/player.tsx
@@ -1,4 +1,4 @@
-import { Center, Text } from "@react-three/drei";
+import { Center, Image, Text } from "@react-three/drei";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Model as Ai } from "./models/cars/Ai";
@@ -8,6 +8,7 @@ import { Model as Himari } from "./models/cars/Himari";
 import { Model as Ouki } from "./models/cars/Ouki";
 import { Model as Renzo } from "./models/cars/Renzo";
 import { Model as Sadako } from "./models/cars/Sadako";
+import { usePlayerState } from "playroomkit";
 
 interface PlayerProps {
     player: any;
@@ -35,6 +36,7 @@ const Player = ({ player, props, car }: PlayerProps) => {
         player.getState("name") || player.getProfile().name
     );
     const [carName, setCarName] = useState(car?.name);
+    const [isReady, _] = usePlayerState(player, "ready", false);
 
     useEffect(() => {
         if (!car) return;
@@ -62,15 +64,41 @@ const Player = ({ player, props, car }: PlayerProps) => {
     return (
         <>
             <Center position={position}>
+                <mesh
+                    position={[0.01, position.y - 0.4, 0]}
+                    rotation={[0, -Math.PI / 2, 0]}
+                >
+                    <planeGeometry args={[playerName.length * 0.06 + 0.2, 0.15]} />
+                    <meshBasicMaterial
+                        color="black"
+                        transparent={true}
+                        opacity={0.5}
+                    />
+                </mesh>
                 <Text
                     fontSize={0.1}
                     color="white"
                     anchorX="center"
                     anchorY="middle"
-                    position={[0, position.y - 0.4, 0]}
+                    position={[0, position.y - 0.4, -0.05]}
                     rotation={[0, -Math.PI / 2, 0]}
                 >
                     {playerName}
+                    {isReady ? (
+                        <Image
+                            url="/icons/green-light.png"
+                            transparent
+                            scale={[0.15, 0.15]}
+                            position={[playerName.length * 0.039, 0, 0]}
+                        /> ) : (
+                            <Image
+                                url="/icons/red-light.png"
+                                transparent
+                                scale={[0.15, 0.15]}
+                                position={[playerName.length * 0.039, 0, 0]}
+                            />
+                        )
+                    }
                 </Text>
                 <Center position-y={0} key={carName}>
                     <CarComponent
