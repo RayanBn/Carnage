9e17982ef3095bc6ff29a636ee324aca126b6b9e
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2025-05-06 12:50:59 +0900

    feat: control car with joystick and PHYSICSSSSSSS

--- client/src/lib/hooks/useVehiclePhysics.ts
+++ client/src/lib/hooks/useVehiclePhysics.ts
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Vector3, Quaternion } from "three";
 import { RapierContext, RapierRigidBody, useRapier } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
@@ -23,17 +23,17 @@ const PHYSICS = {
 };
 
 const suspensionPoints = [
-    new THREE.Vector3(-2, -0.6, -1), // front-left
-    new THREE.Vector3(2, -0.6, -1),  // front-right
-    new THREE.Vector3(-2, -0.6, 1),  // rear-left
-    new THREE.Vector3(2, -0.6, 1)   // rear-right
+    new THREE.Vector3(-2, -0.3, -1), // front-left
+    new THREE.Vector3(2, -0.3, -1),  // front-right
+    new THREE.Vector3(-2, -0.3, 1),  // rear-left
+    new THREE.Vector3(2, -0.3, 1)   // rear-right
 ];
 
 const springData: SpringData = {
-    restLength: 1,
-    stiffness: 5,
-    damping: 25,
-    maxTravel: 0.7
+    restLength: 1.5,
+    stiffness: 2,
+    damping: 20,
+    maxTravel: 1
 };
 
 function radToXY(rad: number) {
@@ -56,11 +56,28 @@ export function useVehiclePhysics(
 
     const suspensionData = useSuspension({ wheelPositions: suspensionPoints, rb: rigidBodyRef, spring: springData });
 
+    const localForwardVec = new THREE.Vector3(1, 0, 0);
+    const localRightVec = new THREE.Vector3(0, 0, 1);
+
+    const [slipVector, setSlipVector] = useState<Vector3>();
+
     useFrame((_, dt) => {
         const rigidBody = rigidBodyRef.current;
         if (!rigidBody) return;
 
         if (isLocalPlayer) {
+
+
+            if (controls?.isJoystickPressed()) {
+                const {x, y} = radToXY(controls.angle());
+                rigidBody.applyImpulseAtPoint(new THREE.Vector3(y, 0, 0).applyQuaternion(rigidBody.rotation()), vec3(rigidBody.translation()).add(new Vector3(0, -1, 0)), true);
+                rigidBody.applyTorqueImpulse(new THREE.Vector3(0, -x * 4, 0), true);
+            }
+
+            const worldRightVec = localRightVec.clone().applyQuaternion(rigidBody.rotation());
+            const slipVel = vec3(rigidBody.linvel()).dot(worldRightVec);
+            rigidBody.applyImpulse(new Vector3(0, 0, -slipVel * 1).applyQuaternion(rigidBody.rotation()), true);
+
             const currentPosition = vec3(rigidBody.translation());
             const currentRotation = quat(rigidBody.rotation());
             emitPositionUpdate(currentPosition, currentRotation);
@@ -99,6 +116,6 @@ export function useVehiclePhysics(
         }
     });
     return ({
-        suspensionData: suspensionData
+        suspensionData: suspensionData,
     });
 }
